ed1d68d06352d7ba558f924a80e75b167c893fc7
Marko Pantelic <marko.pantelic@cubes.rs>
2020-04-20 19:42:48 +0200

    refactor(laravel_lang_sorter): Refactored to CamelCase method and variable names. getopt() implemented. Recursive parsing of directory contents implemented.

--- README.md
+++ README.md
@@ -4,4 +4,132 @@ Misc PHP scripts
 
 ### List ###
 
-* laravel_lang_sorter.php - Sort Laravel's lang file array
+* laravel_lang_sorter.php - Sort and beautify Laravel's lang file. It can be either *.php or *.json file.
+
+
+
+### Manual ###
+
+Basic usage of laravel_lang_sorter.php:
+./laravel_lang_sorter.php /path/to/laravel/lang/file.php
+
+Previously specified command would read, sort, beautify and finally output 
+result to the STDOUT.
+
+To sort and beautify all lang files (to the lowest depth, recursively) from 
+laravel's lang directory, that is to replace old content with new content use:
+./laravel_lang_sorter.php --recursive --inplace /path/to/laravel/lang/folder
+
+If in previous example we remove --recursive option then this command would
+only loop through files on the first level of the specified directory.
+
+Example of .php file contents conversion from e.g.:
+
+<?php
+return [
+    'videos-count' => "Number of videos",
+    'clients-count' => "Number of clients",
+    'livestreams-count' => "Number of livestreams",
+    'artists-count' => "Number of artists",
+    'goto-videos' => "Go to all videos...",
+    'goto-clients' => "Go to all clients...",
+    'goto-livestreams' => "Go to all livestreams...",
+    'goto-artists' => "Go to all artists...",
+    'last-entries-title' => "Last Entries",
+    'info-counts-title' => "Resources / counts",
+    'video-upload-count-chart-title' => "Video upload per month",
+    'video-upload-count-chart-subtitle' => "Videos uploaded by both artists and clients",
+    'clients-created-count-chart-title' => "New clients registered per month",
+    'clients-created-count-chart-subtitle' => "This year's new registered client count",
+    'voting-grade' => "G.",
+    'voting-creators' => "Creators",
+    'voting-title' => "Title",
+    'pie-chart-legend-title' => "Clients by type",
+    'pie-chart-subtitle' => "Ratio of subscribed/premium clients",
+    'last-entry-artist' => "Last artist entry",
+    'last-entry-video' => "Last video entry",
+    'last-entry-livestream' => "Last livestream entry",
+    'last-entry-voting' => "Last finished voting"
+    
+];
+
+to
+
+<?php
+return [
+	'artists-count'                        => 'Number of artists',
+	'clients-count'                        => 'Number of clients',
+	'clients-created-count-chart-subtitle' => 'This year's new registered client count',
+	'clients-created-count-chart-title'    => 'New clients registered per month',
+	'goto-artists'                         => 'Go to all artists...',
+	'goto-clients'                         => 'Go to all clients...',
+	'goto-livestreams'                     => 'Go to all livestreams...',
+	'goto-videos'                          => 'Go to all videos...',
+	'info-counts-title'                    => 'Resources / counts',
+	'last-entries-title'                   => 'Last Entries',
+	'last-entry-artist'                    => 'Last artist entry',
+	'last-entry-livestream'                => 'Last livestream entry',
+	'last-entry-video'                     => 'Last video entry',
+	'last-entry-voting'                    => 'Last finished voting',
+	'livestreams-count'                    => 'Number of livestreams',
+	'pie-chart-legend-title'               => 'Clients by type',
+	'pie-chart-subtitle'                   => 'Ratio of subscribed/premium clients',
+	'video-upload-count-chart-subtitle'    => 'Videos uploaded by both artists and clients',
+	'video-upload-count-chart-title'       => 'Video upload per month',
+	'videos-count'                         => 'Number of videos',
+	'voting-creators'                      => 'Creators',
+	'voting-grade'                         => 'G.',
+	'voting-title'                         => 'Title',
+];
+
+or from contents of JSON lang file e.g:
+
+{
+    "remove": "izbriši",
+    "cancel": "ignoriši izmene",
+    "free": "besplatno",
+    "Artists": "Umetnici",
+    "Live": "Uživo",
+    "Voting": "Glasanje",
+    "Paid": "Plaća se",
+    "Free": "Besplatno",
+    "Videos": "Video",
+    "Top Videos": "Vrh Video",
+    "Live stream": "Live stream",
+    "Comments": "Komentari",
+    "Write a comment": "Napišite Vaš komentar",
+    "view": "pregleda",
+    "Genres": "Žanrovi",
+    "Groups": "Grupe",
+    "Help": "Pomoć",
+    "Notifications": "Obaveštenja",
+    "Overview": "Pregled"
+}
+
+to
+
+{
+    "Artists": "Umetnici",
+    "cancel": "ignoriši izmene",
+    "Comments": "Komentari",
+    "free": "besplatno",
+    "Free": "Besplatno",
+    "Genres": "Žanrovi",
+    "Groups": "Grupe",
+    "Help": "Pomoć",
+    "Live": "Uživo",
+    "Live stream": "Live stream",
+    "Notifications": "Obaveštenja",
+    "Overview": "Pregled",
+    "Paid": "Plaća se",
+    "remove": "izbriši",
+    "Top Videos": "Vrh Video",
+    "Videos": "Video",
+    "view": "pregleda",
+    "Voting": "Glasanje",
+    "Write a comment": "Napišite Vaš komentar"
+}
+
+
+
+
--- laravel_lang_sorter.php
+++ laravel_lang_sorter.php
@@ -1,50 +1,36 @@
 #!/usr/bin/env php
-
 <?php
 
 	/* 
- 	 * ===============================
-	 *
-	 * sorter.php
-	 *
-	 * Sort Laravel's lang file array
-	 * (.php or .json)
-	 *
-	 * author: Marko Pantelic 
-	 *         marko_pantelic@mail.com
-	 * ===============================
+ 	 * ======================================================================= *
+	 *                                                                         *
+	 * laravel_lang_sorter.php                                                 *
+	 *                                                                         *
+	 * Sort Laravel's lang file array (.php or .json) and beautify it's        * 
+	 * contents.                                                               *
+	 *                                                                         *
+	 * author: Marko Pantelic                                                  *
+	 *         marko_pantelic@mail.com                                         *
+	 * ======================================================================= *
 	 */
+
+	// NOTE: This script does not support PHP comments yet!!!!
+	// NOTE: This script outputs only PHP7 style arrays !
 
 	// TODO: catch comments from input file (/**/ and //)
 	//       Check in laravel docs if lang/json can be multilevel.      
-	// 	 receive 'argopt' style input arguments (-i   - inplace file sort
-	// 		                  		 --p5 - php 5 syntax array)
-
-	/*
-		USAGE:
-			sorter.php FILE_TO_SORT [OUTPUT_MODE]
-	 */
 
 
-	define("PROGNAME", "Laravel lang sort");
+	define("PROGNAME", "laravel_lang_sorter");
 	define("INDENT_STR", "\t");
-	define("PHP5_SYNTAX_ARRAY", 0); // not used (will be when argopt is completed)!
-	define("PHP7_SYNTAX_ARRAY", 1); // not used (will be when argopt is completed)!	
-	define("OUTPUT_STDOUT", 2);
-	define("OUTPUT_FILE_ORIGIN", 3);
-
-
-	// choose output syntax array
-	$OUTPUT_SYNTAX_ARRAY = PHP7_SYNTAX_ARRAY;
-
-	// choose output format
-	$OUTPUT = OUTPUT_STDOUT;
+	//define("PHP5_SYNTAX_ARRAY", 0); // TODO: not used
+	//define("PHP7_SYNTAX_ARRAY", 1); // TODO: not used
 
 
 	/*
 	 * Include array from file and return it
 	 */ 
-	function arr_from_file($file_name)
+	function arrFromFile($file_name)
 	{
 		return require $file_name;
 	}	
@@ -52,32 +38,33 @@
 
 	/**
 	 * Sort array recursively by it's keys
-	 * Save max key width for every array level and store results in $levels_max_key_width
+	 * Save max key width for every array level and store results in 
+	 * $levelsMaxKeyWidth variable.
 	 *
-	 * @param $arr                  array   | array to be sorted
-	 * @param $arr_level            integer | level of current recursive step
-	 * @param $levels_max_key_width array   | key = level , value = max key width 
+	 * @param array $arr                | array to be sorted
+	 * @param integer $arrLevel         | level of current recursive step
+	 * @param array $levelsMaxKeyWidth  | key = level , value = max key width 
 	 */ 
-	function recursive_key_sort(&$arr, &$levels_max_key_width, $arr_level=-1)
+	function recursiveKeySort(&$arr, &$levelsMaxKeyWidth, $arrLevel=-1)
 	{
 		ksort($arr, SORT_NATURAL | SORT_FLAG_CASE);
-		$level = $arr_level + 1;
+		$level = $arrLevel + 1;
 
 		// set key to avoid 'undefined offset(key)' error
-		if(! isset($levels_max_key_width[$level])) {
-			$levels_max_key_width[$level] = 0;
+		if(! isset($levelsMaxKeyWidth[$level])) {
+			$levelsMaxKeyWidth[$level] = 0;
 		}
 
 		foreach($arr as $key => $el) {
 
 			// store max key width
 			$kw = strlen($key) + 2;
-			if ($kw > $levels_max_key_width[$level]) {
-				$levels_max_key_width[$level] = $kw;
+			if ($kw > $levelsMaxKeyWidth[$level]) {
+				$levelsMaxKeyWidth[$level] = $kw;
 			}
 
 			if(is_array($el)) {
-				$arr[$key] = recursive_key_sort($el, $levels_max_key_width, $level);
+				$arr[$key] = recursiveKeySort($el, $levelsMaxKeyWidth, $level);
 			}
 			else {
 				$arr[$key] = trim($el);
@@ -87,16 +74,16 @@
 	}
 
 
-	/* 
+	/**
 	 * Recursively construct cumulative string of multidimensional array
 	 *
 	 * @param array $arr                  | array to sort and output as a string
-	 * @param array $levels_max_key_width | array of keys max width for each level
-	 * @param int $arr_level              | current recursive array level
+	 * @param array $levelsMaxKeyWidth    | array of keys max width for each level
+	 * @param int   $arrLevel             | current recursive array level
 	 *
 	 * @return string
 	 */	
-	function arr_str_construct($arr, $levels_max_key_width, $arr_level=0)
+	function arrStrConstruct($arr, $levelsMaxKeyWidth, $arrLevel=0)
 	{
 		$concat = '';
 
@@ -104,15 +91,16 @@
 
 			if(is_array($val)) {
 				 
-				$concat .= ms(INDENT_STR, $arr_level+1);
-				$concat .= sprintf("%-". $levels_max_key_width[$arr_level+1] . "s", "'" . $key . "' => [" . PHP_EOL);
+				$concat .= ms(INDENT_STR, $arrLevel+1);
+				$concat .= sprintf("%-". $levelsMaxKeyWidth[$arrLevel+1] 
+				         . "s", "'" . $key . "' => [" . PHP_EOL);
 
-				$concat .= arr_str_construct($val, $levels_max_key_width, $arr_level+1);
-				$concat .= ms(INDENT_STR, $arr_level+1) . "]," . PHP_EOL;
+				$concat .= arrStrConstruct($val, $levelsMaxKeyWidth, $arrLevel+1);
+				$concat .= ms(INDENT_STR, $arrLevel+1) . "]," . PHP_EOL;
 			}
 			else {
-				$concat .= ms(INDENT_STR, $arr_level+1);
-				$concat .= sprintf("%-". $levels_max_key_width[$arr_level] . "s", "'$key'");
+				$concat .= ms(INDENT_STR, $arrLevel+1);
+				$concat .= sprintf("%-". $levelsMaxKeyWidth[$arrLevel] . "s", "'$key'");
 				$concat .= " => '" . $val . "'," . PHP_EOL;
 			}
 
@@ -135,22 +123,23 @@
 	}
 
 
-	/* 
-	 * Show question and recevie response form user in CLI 
+	/** 
+	 * Show CLI question and receive response form the user.
 	 *
 	 * @param string $question
-	 * @param array $expected
+	 * @param array  $expected
 	 *
 	 * @return string
 	 */
 	function prompt($question, $expected)
 	{
-		// for readline(), libreadline is required
 		/*
+		// for readline(), installed libreadline is required
 		while (! in_array($line = readline($question))) {
 			//readline_add_history($line); // readline library not available in Windows
 		}
 		 */
+
 		$line = '#@$!78-?~'; // most unprobable string
 
 		while( ! in_array($line, $expected) ) {
@@ -162,164 +151,393 @@
 	}
 
 
+	/**
+	 * Print help to the stdout.
+	 * 
+	 * @return void
+	 */
+	function printHelp($optionsExplanation)
+	{
+		echo "Usage: "  . PROGNAME . " [OPTION]... [FILE]... \n";
+		echo "Sort and beautify contents of Laravel lang files\n\n";
 
-
-	// ------ //
-	// main() //
-	// ------ //
-	
-	$result = '';
-
-
-	// check input arguments
-	// ---------------------
-	if ($argc < 2) {
-		echo PROGNAME . ": Invalid number of arguments" . PHP_EOL;
-		echo "USAGE: " . PROGNAME . " FILENAME [OUTPUT_MODE]" . PHP_EOL;
-		echo "output modes: - stdout (default)" . PHP_EOL;
-		echo "              - overwrite (overwrite supplied file with result)" . PHP_EOL;
-
-		exit(1);
-	}
-
-	if ($argc >= 3) {
-
-		switch($argv[2]) {
-			case "stdout":
-				$OUTPUT = OUTPUT_STDOUT; 
-				break;
-			case "overwrite":
-				$OUTPUT = OUTPUT_FILE_ORIGIN;
-				break;
-			default:
-				echo PROGNAME . ": invalid output mode specified. exiting..." . PHP_EOL;
-				exit(1);
+		foreach($optionsExplanation	as $line) {
+			echo $line . "\n";
 		}
 	}
 
-	$file_name = $argv[1];
 
-	// TODO: check file write and read permissions
-	if( ! file_exists($file_name) ) {
-		echo PROGNAME . ": Invalid file path" . PHP_EOL;
-		exit(1);
+	/**
+	 * Check if all option arguments exists.
+	 * getopt() function does not have this feature, therefore this metod was
+	 * created.
+	 * 
+	 * @param array  $allInput
+	 * @param string $shortOptions
+	 * @param array  $longOptions
+	 * 
+	 * @return string|TRUE
+	 */
+	function checkOptionArgumentsExists($shortOptions, $longOptions)
+	{
+		global $argv;
+		$shortOptionsArray = str_split($shortOptions);
+
+		foreach($argv as $arg) {
+
+			if(strlen($arg) == 2 && substr($arg, 0, 1) == '-') { // shortoption
+
+				if(!in_array(ltrim($arg, '-'), $shortOptionsArray)) {
+					return "Unknown short option '$arg'";
+				}
+			}
+			elseif(strlen($arg) > 4 && substr($arg, 0, 2) == '--') { // long option
+
+				if(!in_array(ltrim($arg, '--'), $longOptions)) {
+					return "Unknown long option '$arg'";
+				}
+			}
+		}
+
+		return TRUE;
 	}
-	if( ! is_readable($file_name) ) {	
-		echo PROGNAME . ": Invalid file permissions. File is not readable." . PHP_EOL;
-		exit(1);
+
+
+	/**
+	 * Return non option arguments from $argv array
+	 * 
+	 * @return array
+	 */
+	function getNonOptionArguments()
+	{
+		global $argv;
+		global $argc;
+
+		$argn = 0;
+		while (++$argn < $argc && preg_match('/^-/', $argv[$argn])); # (no loop body)
+
+		$arguments = array_slice($argv, $argn);
+
+		return $arguments;
 	}
 
-	// get supplied filename extension
-	// -------------------------------
-	$filename_parts = explode('.', $file_name);
-	$file_extension = end($filename_parts);
+
+	/**
+	 * Exit execution PHP lang file is invalid.
+	 */
+	function checkPhpLangFile($filepath)
+	{
+		// check if PHP file has comments
+		$contents = file_get_contents($filepath);
+		$hasPhpComments = preg_match('/(\/\*)|(\/\/)/m', $contents); // just look for beginning /* or //
+
+		if($hasPhpComments) {
+			fwrite(STDERR, PROGNAME . ": PHP file has comments. This is currently not supported.\n");
+			return FALSE;
+		}
+
+		return TRUE;
+	}
 
 
-	// preform sort and string operations
-	// ----------------------------------
-	if ($file_extension == 'php') {
+	/**
+	 * Sort PHP array and beautify it. 
+	 * 
+	 * @param string $filepath
+	 * 
+	 * @return string|FALSE
+	 */
+	function performPhpLangFileSortAndBeautify($filepath)
+	{
+		if(!checkPhpLangFile($filepath)) { // this call is temporary until php comments parsing feature is created.
+			return FALSE;
+		}
+
+		$result = '';
+		
 		// store in this array max key width, where array key = level and value = max width
-		$levels_max_key_width = [];
+		$levelsMaxKeyWidth = [];
 
-		$arr_to_sort = arr_from_file($file_name); 
-		recursive_key_sort($arr_to_sort, $levels_max_key_width);
+		$arrToSort = arrFromFile($filepath); 
+		recursiveKeySort($arrToSort, $levelsMaxKeyWidth);
 
-		// print to output
 		$result .= "<?php" . PHP_EOL . PHP_EOL;
 		$result .= "return [" . PHP_EOL;
-
-		$result .= arr_str_construct($arr_to_sort, $levels_max_key_width);
-
+		$result .= arrStrConstruct($arrToSort, $levelsMaxKeyWidth);
 		$result .= "];" . PHP_EOL;
+
+		return $result;
 	}
-	elseif ($file_extension == 'json') {
+
+
+	/**
+	 * Sort JSON object by keys. 
+	 * 
+	 * @param string $filepath
+	 * 
+	 * @return string
+	 */
+	function performJsonLangFileSortAndBeautify($filepath)
+	{
 		// TODO: check if input file is in utf8 (json_decode demands utf8 as input)
 		//       check if JSON syntax is valid. See https://secure.php.net/manual/en/json.constants.php
 		
 		try {
-			$json = file_get_contents($file_name);
+			$json = file_get_contents($filepath);
 		}
 		catch(Exception $e) {
-			echo PROGNAME . ": Reading file error -> " . $e->message;
+			fwrite(STDERR, PROGNAME . ": Reading file error -> " . $e->message);
 			exit(1);
 		}
 
-		$arr_to_sort = json_decode($json, TRUE); // TRUE - convert to associative array
-		ksort($arr_to_sort, SORT_NATURAL | SORT_FLAG_CASE);
+		$arrToSort = json_decode($json, TRUE, JSON_UNESCAPED_UNICODE); // TRUE - convert to associative array
+		ksort($arrToSort, SORT_NATURAL | SORT_FLAG_CASE);
 
-		$result = json_encode($arr_to_sort, JSON_PRETTY_PRINT);
-	}
-	else {
-		echo PROGNAME . ": Invalid file extension detected. Expecting 'php' or 'json'. Exiting..." . PHP_EOL;
-		exit(1);
+		return json_encode($arrToSort, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 	}
 
-	// output result
-	// -------------
-	if ($OUTPUT == OUTPUT_STDOUT) {
-		echo $result;
-	}
-	elseif ($OUTPUT == OUTPUT_FILE_ORIGIN) {
+	/**
+	 * Perform sort and return result as string
+	 * 
+	 * @return string
+	 */
+	function performSortAndBeautify($filepath)
+	{
+		$result = '';
 
-		if( ! is_writable($file_name) ) {	
-			echo PROGNAME . ": Invalid file permissions. File is not writable." . PHP_EOL;
+		// get supplied filename extension
+		// -------------------------------
+		$filenameParts = explode('.', $filepath);
+		$fileExt = end($filenameParts);
+
+		// preform sort and string operations
+		// ----------------------------------
+		if ($fileExt == 'php') {
+			$result = performPhpLangFileSortAndBeautify($filepath);
+		}
+		elseif ($fileExt == 'json') {
+			$result = performJsonLangFileSortAndBeautify($filepath);
+		}
+		else {
+			fwrite(STDERR, PROGNAME . ": Invalid file extension detected. Expecting 'php' or 'json'. Exiting..." . PHP_EOL);
 			exit(1);
 		}
 
-		$answer = prompt("NOTE: All lang file comments will be lost. Continue (yes/no)", ["yes", "no"]);
+		return $result;
+	}
 
-		if($answer == "no") {
-			echo PROGNAME. ": Exiting..." . PHP_EOL;
-			exit(0);
+
+	/**
+	 * Write result to a file.
+	 */
+	function writeResultToFile($filepath, $result, $ask=FALSE)
+	{
+		if( ! is_writable($filepath) ) {	
+			fwrite(STDERR, PROGNAME . ": Invalid file permissions. File is not writable." . PHP_EOL);
+			exit(1);
+		}
+
+		if($ask) {
+			$answer = prompt("NOTE: All old file contents will be overwritten. Continue (yes/no)", ["yes", "no"]);
+
+			if($answer == "no") {
+				echo PROGNAME. ": Exiting..." . PHP_EOL;
+				exit(0);
+			}
 		}
 
 		try {
-			file_put_contents($file_name, $result);
+			file_put_contents($filepath, $result);
 		}
 		catch(Exception $e) {	
-			echo PROGNAME . ": Writing file error -> " . $e->message;
+			fwrite(STDERR, PROGNAME . ": Writing file error -> " . $e->message);
 			exit(1);
 		}
 
-		echo PROGNAME . ": sorting done. Original file updated (overwritten)" . PHP_EOL;
-	}
-	else {
-		echo PROGNAME . ": PROGRAMMER ERROR: invalid 'OUTPUT' option !" . PHP_EOL;
-		exit(1);
+		echo PROGNAME . ": Sorted result written to a file '$filepath'" . PHP_EOL;	
 	}
 
 
-	/*
-	// FIRST SOLUTION
-	$sorted_str = var_export($arr_to_sort, TRUE);
-	$tl = 0; // tab level
-
-	foreach(explode(PHP_EOL, $sorted_str) as $orig_line) {
-
-		$line = trim($orig_line);
-		$tmp = 0;
-		$bonus_tl = '';
-
-		if($line == "array (") {
-			$line = "[";
-			++$tmp;
-			$bonus_tl = INDENT_STR;
-		}
-		elseif($line == "),") {
-			$line = "],";
-			--$tmp;
-		}
-		elseif($line == ");" || $line == ")") {
-			--$tl;
-			$line = "];";
-		}
-		echo ms(INDENT_STR, $tl) . $bonus_tl . $line . PHP_EOL;
-
-		$tl += $tmp;
-	}
+	/**
+	 * Parse CLI arguments and return array with execution setup.
+	 * 
+	 * @return array
 	 */
-	 
-	exit(0);
+	function parseCliArguments()
+	{
+		// inplace and outputTarget are mutually exclusive
+		$execSetup = [
+			'inplace' => NULL,
+			'recursive' => NULL,
+			'output_target' => NULL // TODO: if there is more than one input file than throw error
+		];
 
+		// parse CLI arguments
+		// ----------------------
+		$shortOptions = "hri";
+		$longOptions = ['help', 'recursive', 'inplace'];
+		$optionsExplanation = [
+				"\t -h \t --help \t Print this help message", 
+				"\t -r \t --recursive \t Go through the directory files recursively", 
+				"\t -i \t --inplace \t Sort inplace (overwrite)", 
+		];
+
+		$optArgs = getopt($shortOptions, $longOptions);
+		$nonOptArgs = getNonOptionArguments();
+		$execSetup['non_opt_args'] = $nonOptArgs;
+
+		if( ($res = checkOptionArgumentsExists($shortOptions, $longOptions)) !== TRUE) {
+			fwrite(STDERR, PROGNAME . ': ' . $res . PHP_EOL);
+			exit(1);
+		}
+
+		// go through all known options
+		if( isset($optArgs['h']) || isset($optArgs['help'])) {
+			printHelp($optionsExplanation);
+			exit(1);
+		}
+
+		if( isset($optArgs['i']) || isset($optArgs['inplace'])) {
+			$execSetup['inplace'] = TRUE;
+		}
+
+		if( isset($optArgs['r']) || isset($optArgs['recursive'])) {
+			$execSetup['recursive'] = TRUE;
+		}
+
+		// check if required non option argument is received
+		if(count($nonOptArgs) < 1) {
+			fwrite(STDERR, PROGNAME . ': Filename or directory name is required. Use ' 
+				. '--help option for usage explanation.' . PHP_EOL);
+			exit(1);
+		}
+
+		return $execSetup;
+	}
+
+
+	/**
+	 * Perform required action on one file.
+	 * 
+	 * @param string $filepath
+	 * @param array  $execSetup
+	 * 
+	 * @return boolean
+	 */
+	function performActionOnFile($filepath, $execSetup)
+	{
+		if( ! file_exists($filepath) ) {
+			fwrite(STDERR, PROGNAME . ': Invalid file path' . PHP_EOL);
+			exit(1);
+		}
+		if( ! is_readable($filepath) ) {	
+			fwrite(STDERR, PROGNAME . ': Invalid file permissions. File is not readable.' . PHP_EOL);
+			exit(1);
+		}
+
+		$result = performSortAndBeautify($filepath);
+
+		if($result === FALSE) {
+			fwrite(STDERR, PROGNAME . ": Skipping file '$filepath'" . PHP_EOL);
+			return FALSE;
+		}
+
+		if(isset($execSetup['inplace'])) {
+			writeResultToFile($filepath, $result);
+		}
+		elseif(isset($execSetup['outputTarget'])) {
+			writeResultToFile($execSetup['outputTarget'], $result);
+		}
+		else {
+			echo $result;
+		}
+
+		return TRUE;
+	}
+
+
+	/**
+	 * Perform required actions on directory contents.
+	 * If $execSetup key 'recursive' is set, then perform recursion.
+	 * 
+	 * @param string $filepath
+	 * @param array  $execSetup
+	 * 
+	 * @return void 
+	 */
+	function performActionOnDirectoryContents($dirpath, $execSetup)
+	{
+		if(isset($execSetup['recursive'])) {
+			$dirIterator = new \RecursiveDirectoryIterator($dirpath, 
+												\FilesystemIterator::SKIP_DOTS);
+
+			foreach (new \RecursiveIteratorIterator($dirIterator) as $fileInfo) {
+
+				$filepath = $fileInfo->getPathname();
+				$res = performActionOnFile($filepath, $execSetup);
+			}
+		}
+		else {
+			$dirIterator = new \DirectoryIterator($dirpath); 
+
+			foreach (new \IteratorIterator($dirIterator) as $fileInfo) {
+
+				$filepath = $fileInfo->getPathname();
+
+				if($fileInfo->isDir()) {
+					//echo "Skipping dir " . $filepath . "\n";
+					continue;
+				}
+				//echo $fileInfo->getPathname() . "\n";
+				$res = performActionOnFile($filepath, $execSetup);
+			}
+		}
+
+	}
+	
+
+	/**
+	 * If filepath is dir then recursivly go through files.
+	 * 
+	 * @param string $filepath
+	 * @param array  $execSetup
+	 * 
+	 * @return void
+	 */
+	function pathLooper($filepath, $execSetup)
+	{
+		if(is_dir($filepath) ) {
+			performActionOnDirectoryContents($filepath, $execSetup);
+		}
+		else {
+			performActionOnFile($filepath, $execSetup);
+		}
+	}
+
+
+	/**
+	 * Script's main() function
+	 * 
+	 * @return void
+	 */
+	function main() 
+	{
+		$execSetup = parseCliArguments();
+
+		// loop through all non option arguments and if filepath is a file then 
+		// instantly perform the sort operations. If filepath is a directory then
+		// go through it recursively if --recursive option was set and perform 
+		// sort operation on files.
+		foreach($execSetup['non_opt_args'] as $filepath) {
+			pathLooper($filepath, $execSetup);
+		}
+	}
+
+
+	// ---------- //
+	// run script //
+	// ---------- //
+	main();
+	exit(0);
 
 ?>
